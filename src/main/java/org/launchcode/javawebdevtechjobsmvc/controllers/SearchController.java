4ae33b67128ef4ba1ad92274fa2516e0f07151ef
emebetsissay <61718737+emebetsissay@users.noreply.github.com>
2020-06-15 17:30:34 -0500

    assignment3

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -24,5 +25,16 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    public String search( @RequestParam String mode, @RequestParam String term, Model model){
+        ArrayList<Job> jobs;
+        jobs = JobData.findByColumnAndValue(mode, term);
+        model.addAttribute("type",mode );
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Condition: "+ columnChoices.get(mode) +"Term: "+ columnChoices.get(term));
+        model.addAttribute("jobs",jobs);
+
+
+        return "search.html";
+    }
 
 }
